c592a3953024aed6e3860fef09418d9294cd9830
lizeyang1995 <424011527@qq.com>
2020-09-24 01:25:47 +0800

    [lizeyang] faet: create Header.less

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,19 +1,22 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import './Header.less';
 
 class Header extends Component {
   render() {
     return (
       <div className="header">
         <p>
-          <Link to=''>HOME</Link>
+          <Link to='/'>HOME</Link>
         </p>
         <p>
-          <Link to=''>在线计算器</Link>
+          <Link to='/'>在线计算器</Link>
         </p>
         <p>
-          <Link to=''>在线倒计时器</Link>
+          <Link to='/'>在线倒计时器</Link>
         </p>
       </div>
     );
   }
 }
+export default Header;
