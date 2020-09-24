eb885d0d454955963582c066405abbeeea3adbdc
lizeyang1995 <424011527@qq.com>
2020-09-24 15:57:06 +0800

    [lizeyang] faet: add home style

--- src/app/Home.js
+++ src/app/Home.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './home.less';
+import calculator from  '../images/calculator.png';
+import timer from  '../images/timer.png';
 
 const Home = () => {
   return (
     <div className="home">
-      This is home page
+      <section className='shutter'>
+        <h1>在线实用工具</h1>
+      </section>
+      <section className='features'>
+        <div>
+          <img src={calculator} alt='calculator'></img>
+          <h1>计算器</h1>
+        </div>
+        <div>
+          <img className='timer' src={timer} alt='timer'></img>
+          <h1>倒计时器</h1>
+        </div>
+      </section>
     </div>
   );
 };
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
-import './Header.less';
+import './header.less';
 
 class Header extends Component {
   render() {
